7b0a1f4ebff61909d4c19551db6f8a5128d63a61
Marcus Baltazar <mvabf_dev@outlook.com>
2022-02-10 23:22:35 -0300

    kafka added

---
+++ src/app.module.ts
@@ -0,0 +1,31 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ScheduleModule } from '@nestjs/schedule';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TweetsModule } from './tweets/tweets.module';
+import { MailListModule } from './mail-list/mail-list.module';
+import { BullModule } from '@nestjs/bull';
+
+@Module({
+  imports: [
+    BullModule.forRoot({
+      redis: {
+        host: 'redis',
+        port: 6379,
+      },
+    }),
+    TweetsModule,
+    MongooseModule.forRoot(
+      'mongodb://root:root@db/analytics?authSource=admin',
+      {
+        useNewUrlParser: true,
+      },
+    ),
+    ScheduleModule.forRoot(),
+    MailListModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
